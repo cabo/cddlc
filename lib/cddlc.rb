795aca30299f8a2c70cc45e284391ea648e0b932
Carsten Bormann <cabo@tzi.org>
2023-02-06 04:08:00 +0100

    move include/import processing to lib/cddlc.rb

--- lib/cddlc.rb
+++ lib/cddlc.rb
@@ -3,6 +3,14 @@ require_relative "parser/cddl-util.rb"
 class CDDL
   @@parser = CDDLGRAMMARParser.new
 
+  DATA_DIR = Pathname.new(__FILE__) + "../../data/"
+  # empty string is for CDDL::DATA_DIR
+  CDDL_INCLUDE_PATH = ENV["CDDL_INCLUDE_PATH"] || ".:"
+
+  def self.cddl_include_path
+    CDDL_INCLUDE_PATH.split(":", -1).map {_1 == "" ? CDDL::DATA_DIR : Pathname.new(_1)}
+  end
+
   def self.reason(parser, s)
     reason = [parser.failure_reason]
     parser.failure_reason =~ /^(Expected .+) after/m
@@ -14,20 +22,104 @@ class CDDL
     reason.join("\n")
   end
 
+  SAFE_FN = /\A[-._a-zA-Z0-9]+\z/
+
   def self.from_cddl(s)
     ast = @@parser.parse s
     if !ast
       fail self.reason(@@parser, s)
     end
-    CDDL.new(ast)
+    if $options.cddl2
+      directives = s.lines.grep(/^;# /).map(&:chomp).map{|l| l.sub(/^;#\s+/, '').split(/\s+/)}
+      # puts directives.to_yaml
+    end
+    ret = CDDL.new(ast, directives)
+
+    if $options.cddl2
+      ret.directives.each do |di|
+        preferred_tag = nil
+        case di
+        in ["include" => dir, docref]
+        in ["include" => dir, docref, "as", preferred_tag]
+          warn "** Ignoring namespace tag #{preferred_tag} for now"
+        in ["import" => dir, docref]
+        else
+          warn "** Can't parse include directive #{di.inspect}"
+          next
+        end
+        unless docref =~ SAFE_FN
+          warn "** skipping unsafe filename #{docref}"
+          next
+        end
+        puts "PREFERRED_TAG #{preferred_tag}" if $options.verbose
+        puts "DOCREF #{docref}" if $options.verbose
+        fn = docref.downcase << ".cddl"
+
+        io = nil
+        CDDL::cddl_include_path.each do |path|
+          begin
+            io = (path + fn).open
+            break
+          rescue Errno::ENOENT
+            next
+          end
+        end
+        unless io
+          warn "** include file #{fn} not found in #{CDDL::cddl_include_path.map(&:to_s)}"
+          next
+        end
+
+        include_file = io.read
+        included_cddl = CDDL.from_cddl(include_file)
+        # XXX: Should namespace that thing now! all names -> preferred_tag.name
+        # p included_cddl.rules.keys
+        # XXX: do once, first, to rename COSE_Key to RFC9052.COSE_Key
+        case dir
+        in "import"
+          warn "** IMPORTING #{fn}" if $options.verbose
+          require_relative '../lib/processor/cddl-undefined.rb'
+          loop do
+            undef_rule = ret.cddl_undefined # XXX square...
+            # p undef_rule
+            got_more = false
+            undef_rule.each do |name|
+              if rule = included_cddl.rules[name]
+                ret.rules[name] = rule                     # XXX must be namespaced!
+                warn "IMPORTED #{name} from #{fn}" if $options.verbose # ...: #{rule}...
+                got_more = true
+              end
+            end
+            break unless got_more
+          end
+        in "include"
+          warn "** INCLUDING #{fn}" if $options.verbose
+          included_cddl.rules.each do |k, v|
+            if old = ret.rules[k]
+              if old != v
+                warn "** included rule #{k} = #{v} would overwrite #{old}"
+              end
+            else
+              ret.rules[k] = v
+            end
+          end
+        end
+      end
+    end
+    ret
   end
 
-  attr_accessor :ast, :tree
-  def initialize(ast_)
+  attr_accessor :ast, :tree, :directives
+  def initialize(ast_, directives_ = [])
     @ast = ast_
     @tree = ast.ast
     @rules = nil                # only fill in if needed
+    @directives = directives_
   end
+
+  def deep_clone
+    Marshal.load(Marshal.dump(self))
+  end
+
 
   RULE_OP_TO_CHOICE = {"/=" => "tcho", "//=" => "gcho"}
 
@@ -65,8 +157,6 @@ class CDDL
     end
     @rules
   end
-
-  DATA_DIR = Pathname.new(__FILE__) + "../../data/"
 
   def prelude
     if @prelude.nil?
