5f129481a86d844704de2da329b6992b893e0a22
Carsten Bormann <cabo@tzi.org>
2023-02-06 04:12:12 +0100

    Clean up cddl_undefined

--- lib/processor/cddl-undefined.rb
+++ lib/processor/cddl-undefined.rb
@@ -3,27 +3,36 @@ require_relative "./cddl-visitor.rb"
 require_relative "./cddl-expander.rb"
 
 class CDDL
-  def cddl_undefined
-    # currently only works on expanded...
-    cddl2 = self
-    cddl2.expand_generics
-    used = {}
-    gen_used = {}
-    def_gen = {}
-    cddl2.rules.each do |k, v|
-      def_gen[k[0]] = true if Array === k
-      cddl2.visit(v) do |p, &block|
-        case p
+  def cddl_extract_names(prod, &name_used)
+    visit(prod) do |p|
+      case p
         in ["gen", name, *_gen_args]
-          gen_used[name] = true
+          name_used.call(name)
+          false
         in ["name", name]
-          used[name] = true
+          name_used.call(name)
+          false
         else
           false
-        end
       end
     end
-    [used.keys.reject {|name| cddl2.rules[name] || prelude.rules[name]},
-     gen_used.keys.reject {|name| def_gen[name] }]
+  end
+
+  def cddl_add_used_by(prod, used)
+    cddl_extract_names(prod) do |name|
+      used[name] = true
+    end
+  end
+
+  def cddl_undefined
+    # currently only works on expanded...
+    cddl2 = self.deep_clone                # needs deep-clone
+    cddl2.expand_generics
+    used = {}
+    cddl2.rules.each do |k, v|
+      fail unless String === k
+      cddl2.cddl_add_used_by(v, used)
+    end
+    used.keys.reject {|name| cddl2.rules[name] || prelude.rules[name]}
   end
 end
