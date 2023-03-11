e4d257f424ef83c28126c8224e6184d6c27c8953
Carsten Bormann <cabo@tzi.org>
2023-03-11 16:49:20 +0100

    First prototype of flattening

--- lib/processor/cddl-flattening.rb
+++ lib/processor/cddl-flattening.rb
@@ -2,42 +2,26 @@ require_relative "../cddlc.rb"
 require_relative "./cddl-visitor.rb"
 
 class CDDL
-  def flatten_prod(prod)
-    visit(prod) do |p, &block|
-      case p
-      in ["gen", name, *gen_args]
-        [true, gen_apply(name, gen_args, &block)]
-      else
-        [false]
-      end
-    end
-  end
+  ID_RE = /\A[A-Za-z@_$]([-.]*[A-Za-z@_$0-9])*\z/
+  MOGRIFIED_ID_RE = /\A\$\.[A-Za-z@_$]([-.]*[A-Za-z@_$0-9])*\z/
   def flattening_key_name(key, value)
     case key
-    in nil
-    in ["enum", enum]
-      warn "*** ENUM #{enum.inspect}"
-    in ["text", text]
-      # symtab[text] += 1
+    in ["enum", ["mem", ["text", ID_RE => text], _]]
+      text
+    in ["text", ID_RE => text]
       text
     else
     end
   end
   def flattening_occurrences
-    symtab = Hash.new(0)
+    symtab = Hash.new { |h, k| h[k] = [] }
     rules.each do |name, prod|
       visit(prod) do |here|
         case here
         in ["mem", key, value]
-          ### mogrify
           keyname = flattening_key_name(key, value)
           if keyname
-            # Todo: check whether this even needs a flattening; e.g., unique names should be fine
-            # case value
-            # in ["name", _name]
-            # else
-              symtab[keyname] += 1
-            # end
+            symtab[keyname] << [name, keyname]
             false
           end
         else
@@ -47,8 +31,51 @@ class CDDL
     end
     symtab
   end
+  def flattening_mogrify(name, prod, symtab, alias_rules)
+    step1 = visit(prod) do |here|
+        case here
+        in ["mem", key, value]
+          ### mogrify
+          keyname = flattening_key_name(key, value)
+          if keyname
+            syment = symtab[keyname]
+            fail keyname unless Array === syment
+            new_name =
+              if syment.size == 1
+                "$.#{keyname}"
+              else
+                "$.#{name}$#{keyname}"
+              end
+            new_value2 = flattening_mogrify(new_name, value, symtab, alias_rules)
+            fail [alias_rules, new_name].inspect if alias_rules[new_name] # XXX
+            alias_rules[new_name] = new_value2
+            [true, ["mem", key, ["name", new_name]]]
+          end
+        else
+          false
+        end
+      end
+    step2 = visit(step1) do |here|
+      case here
+      in ["enum", ["mem", ["text", ID_RE], ["name", MOGRIFIED_ID_RE => new_name]]]
+        [true, ["name", new_name]]
+      else
+        false
+      end
+    end
+    step2
+  end
+  def flattening_replace(symtab)
+    alias_rules = {}
+    new_rules = Hash[rules.map do |name, prod|
+                       [name,
+                        flattening_mogrify(name, prod, symtab, alias_rules)]
+                     end]
+    new_rules.merge(alias_rules)
+  end
   def flattening
     symtab = flattening_occurrences
-    warn "*** SYMTAB #{symtab.inspect}"
+    # warn "*** SYMTAB #{symtab.inspect}"
+    rules.replace(flattening_replace(symtab))
   end
 end
